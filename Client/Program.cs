8e68e9905659e267ec5d27da681d513e7ee012a3
Севастьян Безгодков <sevabezgodkov5@gmail.com>
2024-04-30 15:37:50 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var thread = new Thread(async () => await CreateClient());
+            thread.Start();
+
+            await CreateClient();
+            thread.Join();
+        }
+
+        static async Task CreateClient()
+        {
+            var words = new string[] { "red", "yellow", "blue", "green" };
+
+            using TcpClient tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync("127.0.0.1", 8888);
+            var stream = tcpClient.GetStream();
+
+            var response = new List<byte>();
+            int bytesRead = 10; 
+            foreach (var word in words)
+            {
+
+                byte[] data = Encoding.UTF8.GetBytes(word + '\n');
+                await stream.WriteAsync(data);
+
+                while ((bytesRead = stream.ReadByte()) != '\n')
+                {
+                    response.Add((byte)bytesRead);
+                }
+                var translation = Encoding.UTF8.GetString(response.ToArray());
+                Console.WriteLine($"Слово {word}: {translation}");
+                response.Clear();
+
+                await Task.Delay(2000);
+            }
+
+            await stream.WriteAsync(Encoding.UTF8.GetBytes("END\n"));
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var tcpListener = new TcpListener(IPAddress.Any, 8888);
+
+try
+{
+    tcpListener.Start();   
+    Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+
+    while (true)
+    {
+        var tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+        Task.Run(async () => await ProcessClientAsync(tcpClient));
+    }
+}
+finally
+{
+    tcpListener.Stop();
+}
+
+async Task ProcessClientAsync(TcpClient tcpClient)
+{
+    var words = new Dictionary<string, string>()
+    {
+        {"red", "красный" },
+        {"blue", "синий" },
+        {"green", "зеленый" },
+    };
+    var stream = tcpClient.GetStream();
+
+    var response = new List<byte>();
+    int bytesRead = 10;
+    while (true)
+    {
+        while ((bytesRead = stream.ReadByte()) != '\n')
+        {
+            response.Add((byte)bytesRead);
+        }
+        var word = Encoding.UTF8.GetString(response.ToArray());
+
+        if (word == "END") break;
+
+        Console.WriteLine($"Клиент {tcpClient.Client.RemoteEndPoint} запросил перевод слова {word}");
+
+        if (!words.TryGetValue(word, out var translation)) translation = "не найдено в словаре";
+
+        translation += '\n';
+
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(translation));
+        response.Clear();
+    }
+    tcpClient.Close();
+}
